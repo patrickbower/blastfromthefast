fdd1a0a9debcbcfb089f1f71bbec7a3f35381697
Patrick Bower <patrickbower@Patricks-MacBook-Pro.local>
2017-05-10 15:09:52 +0100

    returning strings

--- scripts.js
+++ scripts.js
@@ -17,8 +17,9 @@ function getLocation() {
 }
 
 function success(position) {
-  let speed = Math.round(position.coords.speed * 100) / 100;
-  speedReadOut.innerHTML = speed ? (speed * 2.2369) + 'mph' : 'fetching';
+  let speedCovert = position.coords.speed * 2.2369;
+  let speed = Math.round(speedCovert * 100) / 100;
+  speedReadOut.innerHTML = speed ? speed + 'mph' : 'fetching';
   latReadOut.innerHTML = position.coords.latitude ? position.coords.latitude : 'fetching';
   longReadOut.innerHTML = position.coords.longitude ? position.coords.longitude : 'fetching';
   calcTopSpeed(speed);
@@ -31,11 +32,33 @@ function error(positionError) {
 function stop() {
   console.log('stopped');
   navigator.geolocation.clearWatch(watchId);
-  speedReadOut.innerHTML = 'top speed ' + (topSpeed * 2.2369) + 'mph';
+  let speedType = returnSpeedType(topSpeed);
+
+  speedReadOut.innerHTML = 'top speed ' +  topSpeed + 'mph' +
+                            '<br/>' +
+                            speedType;
 }
 
 function calcTopSpeed(speed) {
   if (speed > topSpeed) topSpeed = speed;
+}
+
+function returnSpeedType(topSpeed) {
+  var x = topSpeed;
+  switch (true) {
+    case (x < 2):
+        return 'Yo, that was slow bro';
+        break;
+    case (x < 4):
+        return 'you were steady Eddy';
+        break;
+    case (x < 8):
+        return 'you were fast as lightning';
+        break;
+    default:
+        return 'what the hell happened out there?'
+        break;
+  }
 }
 
 document.getElementById('button-start').addEventListener('click', function(event){
